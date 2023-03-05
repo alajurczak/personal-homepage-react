a24b7f4c50a03bf48860f826e571358a11566619
Alicja Jurczak <ala.jurczak@gmail.com>
2023-03-05 20:28:58 +0100

    gap between Portfolio and Footer has been corrected

--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     margin-top: 72px;
-    margin-bottom: 24px;
+    margin-bottom: 120px;
     display: flex;
     flex-direction: column;
     align-items: center;
