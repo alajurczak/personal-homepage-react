ea7acdac655bc0ac076aa25010a37d4b906a7357
Alicja Jurczak <ala.jurczak@gmail.com>
2023-03-05 21:54:57 +0100

    correction of misspelled word

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -6,7 +6,7 @@ import {ReactComponent as InstagramLogo} from "./logos/InstagramLogo.svg";
 
 export const Footer = () => (
     <Wrapper>
-        <Caption>LETS'S TALK</Caption>
+        <Caption>LET'S TALK</Caption>
         <Email>ala.jurczak@gmail.com</Email>
         <Description>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
