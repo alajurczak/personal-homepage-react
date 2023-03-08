3012e633e52a5828a7afaf2b8fa626442392af2e
Alicja Jurczak <ala.jurczak@gmail.com>
2023-03-08 17:50:55 +0100

    media query to Portfolio added

--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -6,7 +6,11 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    color: ${({theme}) => theme.colors.mineShaft};
+    color: ${({ theme }) => theme.colors.mineShaft};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+        margin-bottom: 48px;
+    }
 `;
 
 export const Title = styled.h2`
@@ -15,11 +19,16 @@ export const Title = styled.h2`
     line-height: 36px;
     margin-top: 12px;
     margin-bottom: 8px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+        font-size: 18px;
+        line-height: 22px;
+    }
 `;
 
 export const Description = styled.p`
     font-weight: 400;
-    font-size: 32spx;
+    font-size: 20px;
     line-height: 140%;
     margin: 0;
     margin-bottom: 24px;
