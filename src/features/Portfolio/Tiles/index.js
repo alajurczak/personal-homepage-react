ddb0d68dc292a1799fadc17276d7e027b9d033c0
Alicja Jurczak <ala.jurczak@gmail.com>
2023-05-06 19:09:55 +0200

    repository filtering added

--- src/features/Portfolio/Tiles/index.js
+++ src/features/Portfolio/Tiles/index.js
@@ -3,36 +3,42 @@ import { Wrapper, Tile, Title, Descripion, LinkWrapper, Link } from "./styled";
 import { useSelector } from "react-redux";
 
 export const Tiles = () => {
-    const repositories = useSelector(selectRepositories);
+  const repositories = useSelector(selectRepositories);
+  const unwantedRepos = ["alajurczak", "personal-homepage-react"];
+  const filteredRepos = repositories.filter(
+    ({ name }) => !unwantedRepos.includes(name)
+  );
 
-    return (
-        <Wrapper>
-            {repositories.map(repo => (
-                <Tile key={repo.id}>
-                    <Title>{repo.name}</Title>
-                    <Descripion>{repo.description}</Descripion>
+  return (
+    <Wrapper>
+      {filteredRepos.map((repo) => (
+        <Tile key={repo.id}>
+          <Title>{repo.name}</Title>
+          <Descripion>{repo.description}</Descripion>
 
-                    <LinkWrapper>
-                        Demo:
-                        <Link
-                            href={`https://${repo.owner.login}.github.io/${repo.name}`}
-                            title={`https://${repo.owner.login}.github.io/${repo.name}`}
-                            target="_blank"
-                            rel="noreferrer noopener">
-                            https://link.demo.com
-                        </Link>
-                    </LinkWrapper>
-                    <LinkWrapper>
-                        Code:
-                        <Link
-                            href={repo.html_url}
-                            target="_blank"
-                            rel="noreferrer noopener">
-                            https://link.code.com
-                        </Link>
-                    </LinkWrapper>
-                </Tile>
-            ))}
-        </Wrapper>
-    )
+          <LinkWrapper>
+            Demo:
+            <Link
+              href={`https://${repo.owner.login}.github.io/${repo.name}`}
+              title={`https://${repo.owner.login}.github.io/${repo.name}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              https://link.demo.com
+            </Link>
+          </LinkWrapper>
+          <LinkWrapper>
+            Code:
+            <Link
+              href={repo.html_url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              https://link.code.com
+            </Link>
+          </LinkWrapper>
+        </Tile>
+      ))}
+    </Wrapper>
+  );
 };
